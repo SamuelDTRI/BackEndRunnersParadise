b6633eba72eb266a2d6b9e32fbf179e0d6fb9f1e
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-08 18:53:18 -0500

    test cloudinary.v2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
+const multer = require("multer")
+const cloudinary = require("cloudinary").v2
+
+const upload = multer({dest: "uploads"})
+
 const port = 3000
+
 
 app.get("/",(req,res) => {
     res.send("prueba")
+})
+
+app.post("/upload",upload.single("RunnersParadise"),async(req,res)=>{
+    try{
+        const result = await cloudinary.uploader.upload(req.file.path)
+        res.status(200).json(result)
+    }catch(error){
+        console.log("error",error)
+        res.status(400).send(error.message)
+    }
 })
 
 app.listen(port,()=>{
