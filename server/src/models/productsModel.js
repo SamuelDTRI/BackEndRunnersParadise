1df38e6da204dfcc4d986ce8e3a447e7ce3b82e5
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-09 13:26:11 -0500

    module.exports

--- server/src/models/productsModel.js
+++ server/src/models/productsModel.js
@@ -1,34 +1,33 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-const Product = sequelize.define('Product', { 
+module.exports = (sequelize) => {
+  sequelize.define("Product", {
     name: {
-        type: DataTypes.STRING,
-        unique:true,
-        allowNull: false,
-      },
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey: true,
-      },
-      price:{
-        type:DataTypes.NUMBER,
-        allowNull:false
-      },
-      colors:{
-        type:DataTypes.STRING,
-        allowNull:false
-      },
-      size:{
-        type:DataTypes.NUMBER,
-        allowNull:false
-      },
-      image:{
-        public_id: STRING,
-        secure_url:STRING,
-      },
- });
-
-
-
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    price: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    colors: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    size: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    image: {
+      public_id: STRING,
+      secure_url: STRING,
+    },
+  });
+};
