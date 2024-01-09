21323c229fa72dd722ec36b10017e3a95df4273d
tomaspon <tomaspon11@gmail.com>
2024-01-09 16:40:48 -0300

    feature(server/model) usersModel

--- server/src/models/usersModel.js
+++ server/src/models/usersModel.js
@@ -1,3 +1,37 @@
-const { DataTypes } = require('sequelize');
-const User = sequelize.define('User', { 
-    /* campos del review */ })
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      surName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false, freezeTableName: true }
+  );
+};
