001373c4272769ae10a73d84ce317f80d8f3c1ee
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-09 13:13:28 -0500

    update

--- server/src/controllers/postProductsDb.js
+++ server/src/controllers/postProductsDb.js
@@ -1,3 +1,32 @@
+const axios = require("axios");
+
+const products = async (req,res) => {
+    try{
+        const response = await axios.get("http://localhost:5000/sneakers")
+        const data = response.data;
+        const result = data.map( country =>{
+            return {
+            id:country.cca3,
+            name:country.name.common,
+            image: country.flags.png,
+            continente: Array.isArray(country.continents) ? country.continents.join(", ") : country.continents,
+            capital: Array.isArray(country.capital) ? country.capital.join(", ") : (country.capital || 'Valor Predeterminado'),
+            subregion: country.subregion,
+            area: country.area,
+            poblacion: country.population,
+          }});
+        const dbCountry= await Country.bulkCreate(result)
+        console.log("se ingresaron correctamente")
+        return  res.status(200).json(dbCountry);
+    }catch(error){
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    paises
+}
+/*
 const cloudinary = require("cloudinary").v2
 const multer = require("multer")
 
@@ -6,9 +35,13 @@ const upload = multer({dest: "uploads"})
 upload.single("RunnersParadise"),async(req,res)=>{
     try{
         const result = await cloudinary.uploader.upload(req.file.path)
+        product.image = {
+            public_id:result.public_id,
+            secure_url:result.secure_url
+        }
         res.status(200).json(result)
     }catch(error){
         console.log("error",error)
         res.status(400).send(error.message)
     }
-}
+}*/
