613832a23da94b054c945e08beacc2826b4312e6
sariditri <sariditri31@gmail.com>
2024-01-08 21:07:58 -0500

    feature(server/index)
    full database connection for now

--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
+const express = require("express");
+const { database } = require("./src/db");
+const server = require("./src/server");
+require("dotenv").config();
 
-const express = require("express")
-const app = express()
-require("dotenv").config()
+const port = 3000;
 
-
-const port = 3000
-
-
-
-
-
-app.listen(port,()=>{
-    console.log("running in port", port)
-})
-
+database.sync({ alter: true }).then(
+  server.listen(port, () => {
+    console.log("running in port", port);
+  })
+);
