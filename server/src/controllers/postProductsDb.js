dbdf675b20fc5842b5943a05a787807fd9e9fd22
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-10 11:35:41 -0500

    update files and change post

--- server/src/controllers/postProductsDb.js
+++ server/src/controllers/postProductsDb.js
@@ -1,7 +1,9 @@
 const { Product } = require("../db");
 
 const postProduct = async (name, size, price, colors, image) => {
-  const postInDb = await Product.create({ name, size, price, colors, image });
+    const maxId = await Product.max('id');
+    const newId = maxId + 1;
+  const postInDb = await Product.create({ id:newId, name, size, price, colors, image });
   return postInDb;
 };
 
