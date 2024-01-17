925cdf4c42d404cccb059c5f1db870c333bbbe96
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-17 10:06:39 -0500

    Deploy database return

--- server/src/db.js
+++ server/src/db.js
@@ -5,21 +5,27 @@ const reviewsModel = require("./models/reviewsModel");
 const usersModel = require("./models/usersModel");
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false,
+    native: false,
+  }
+);
 
 productsModel(sequelize);
 reviewsModel(sequelize);
 usersModel(sequelize);
 
-const { Review, Product, User } = sequelize.models;
+const { Review, Product, User  } = sequelize.models
 
-Product.hasMany(Review, { foreignKey: "productId", as: "review" });
-Review.belongsTo(Product, { foreignKey: "productId", as: "review" });
+Product.hasMany(Review, { foreignKey: 'productId', as: 'review' });
+Review.belongsTo(Product, { foreignKey: 'productId',  as: 'review' });
 
-User.hasMany(Review, { foreignKey: "userId" });
-Review.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(Review, { foreignKey: 'userId' });
+Review.belongsTo(User, { foreignKey: 'userId' });
 
-module.exports = { sequelize, Product, Review, User };
+module.exports = { sequelize, Product,Review,User };
+
+
+
