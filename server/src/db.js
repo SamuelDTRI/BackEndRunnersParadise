72e37f9a3a7a44f1f15e0109cd4fcbbb028bde0a
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-17 09:50:37 -0500

    Deploy database

--- server/src/db.js
+++ server/src/db.js
@@ -1,31 +1,25 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 const productsModel = require("./models/productsModel");
 const reviewsModel = require("./models/reviewsModel");
 const usersModel = require("./models/usersModel");
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+});
 
 productsModel(sequelize);
 reviewsModel(sequelize);
 usersModel(sequelize);
 
-const { Review, Product, User  } = sequelize.models
+const { Review, Product, User } = sequelize.models;
 
-Product.hasMany(Review, { foreignKey: 'productId', as: 'review' });
-Review.belongsTo(Product, { foreignKey: 'productId',  as: 'review' });
+Product.hasMany(Review, { foreignKey: "productId", as: "review" });
+Review.belongsTo(Product, { foreignKey: "productId", as: "review" });
 
-User.hasMany(Review, { foreignKey: 'userId' });
-Review.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Review, { foreignKey: "userId" });
+Review.belongsTo(User, { foreignKey: "userId" });
 
-module.exports = { sequelize, Product,Review,User };
-
-
-
+module.exports = { sequelize, Product, Review, User };
