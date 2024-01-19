d05711cbbc57717eedc9a4b862a8345ab9f4f6c9
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-01-18 21:52:56 -0500

    cloudinary upload and delete

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
+const fileUpload = require ("express-fileupload")
+
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
-
+server.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : './uploads'
+}));
 server.use("/",router);
 
 module.exports = server;
