37fecb3d7591fe9255290b84500caa14cd88ddf4
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-19 00:13:21 -0500

    forrr

--- server/src/db.js
+++ server/src/db.js
@@ -1,24 +1,17 @@
 require("dotenv").config();
-const { DB_DEPLOY } = process.env;
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = process.env;
 const productsModel = require("./models/productsModel");
 const reviewsModel = require("./models/reviewsModel");
 const usersModel = require("./models/usersModel");
 const { Sequelize } = require("sequelize");
 
-console.log(`Database Deployed URL: ${DB_DEPLOY}`);
-
-const sequelize = new Sequelize(DB_DEPLOY, {
-  dialect: "postgres",
-  ssl: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // self-signed certificates. :)
-    },
-  },
-  logging: false, // Here we are disabling the printing of log messages (in the console).
-  native: false, // Here we are choosing not to use the native driver (we have the JavaScript one).
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // Here we are disabling the printing of log messages (in the console).
+    native: false, // Here we are choosing not to use the native driver (we have the JavaScript one).
+  }
+);
 
 productsModel(sequelize);
 reviewsModel(sequelize);
