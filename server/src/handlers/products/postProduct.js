959491dac9e6658a82986cb65dac6aea1182ff75
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-19 12:45:33 -0500

    ok

--- server/src/handlers/products/postProduct.js
+++ server/src/handlers/products/postProduct.js
@@ -4,7 +4,7 @@ const postProductsHandler = async (req, res) => {
   const { name, size,brand, price, colors ,image} = req.body;
   try {
     const response = await postProduct(name, size, brand, price, colors,req,image);
-    res.status(200).json(response);
+    res.status(201).json(response);
   } catch (error) {
     res.status(404).send("Error while creating the product, please check your information again.");
   }
