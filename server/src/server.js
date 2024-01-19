ef67ebf03f79a998b6475f6b87255b94edf0123b
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-19 11:50:14 -0500

    deploy succes

--- server/src/server.js
+++ server/src/server.js
@@ -4,41 +4,41 @@ const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-// Función para enviar el correo
-const sendEmail = async () => {
-  const config = {
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-      user: "sariditri31@gmail.com",
-      pass: "xkli ntae gvjf uxcd", // Corregí "passw" a "pass"
-    },
-  };
+// // Función para enviar el correo
+// const sendEmail = async () => {
+//   const config = {
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     auth: {
+//       user: "sariditri31@gmail.com",
+//       pass: "xkli ntae gvjf uxcd", // Corregí "passw" a "pass"
+//     },
+//   };
 
-  const message = {
-    from: "sariditri31@gmail.com",
-    to: "@gmail.com",
-    subject: "correo de pruebas",
-    text: "Envio de correo desde node.js utilizando nodemailer",
-  };
+//   const message = {
+//     from: "sariditri31@gmail.com",
+//     to: "@gmail.com",
+//     subject: "correo de pruebas",
+//     text: "Envio de correo desde node.js utilizando nodemailer",
+//   };
 
-  // Crear el objeto de transporte fuera de la función sendEmail
-  const transport = nodemailer.createTransport(config);
+//   // Crear el objeto de transporte fuera de la función sendEmail
+//   const transport = nodemailer.createTransport(config);
 
-  try {
-    const info = await transport.sendMail(message);
-    console.log(info);
-  } catch (error) {
-    console.error("Error al enviar el correo:", error);
-  } finally {
-    // Cerrar el transporte después de enviar el correo
-    transport.close();
-  }
-};
+//   try {
+//     const info = await transport.sendMail(message);
+//     console.log(info);
+//   } catch (error) {
+//     console.error("Error al enviar el correo:", error);
+//   } finally {
+//     // Cerrar el transporte después de enviar el correo
+//     transport.close();
+//   }
+// };
 
-sendEmail();
+// sendEmail();
 
 const server = express();
 
