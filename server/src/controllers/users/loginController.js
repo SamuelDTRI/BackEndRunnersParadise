6b6e6f81b7d98d40eaf32a49970d2e36075085e9
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-01-22 23:01:24 -0300

    login usuarios registrados

---
+++ server/src/controllers/users/loginController.js
@@ -0,0 +1,32 @@
+const { User } = require('../../db');
+
+ const login= async (req, res) => {
+ const { email, password } = req.body;
+ const user = await User.findOne({ where: { email : email } });
+ if (!user) {
+    return res.status(400).send({ message: 'Usuario no existe' });
+ }
+
+ 
+ const validPassword = password === user.password;
+ 
+ if (validPassword) {
+    return res.status(400).send({ message: 'ContraseÃ±a incorrecta' });
+ }
+
+ if(!validPassword){
+   const response = {
+      id:user.id,
+      name:user.name,
+      surName:user.surName,
+      email:user.email
+   }
+   return res.json(response);
+ }
+
+ 
+};
+
+module.exports = {
+    login
+}
--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -3,9 +3,11 @@ const allUsersHandler = require("../handlers/users/allUsersHandler");
 const postUsersHandler = require("../handlers/users/postUsersHandler")
 const usersById = require("../handlers/users/usersById")
 const validateUsers=require("../middlewares/users/usersMiddleware");
+const {login} = require("../controllers/users/loginController")
 const usersRouter = Router();
 
 usersRouter.get("/", allUsersHandler);
 usersRouter.post("/create",validateUsers, postUsersHandler);
+usersRouter.post("/login", login);
 usersRouter.get("/detail/:idKey", usersById);
 module.exports = usersRouter;
