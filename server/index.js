886d69a54e05563b765b41d2e7b261473ea2565f
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-25 20:58:27 -0500

    ok

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { sequelize } = require("./src/db");
 const server = require("./src/server");
 
-sequelize.sync({ alter: true }).then(
+sequelize.sync({ force: false }).then(
   server.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000 }`);
   })
