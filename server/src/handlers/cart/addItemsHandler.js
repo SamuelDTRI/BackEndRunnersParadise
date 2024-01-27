862e3cd8a7779e067b055446c3e83023001c9a3a
sariditri31@gmail.com <sariditri31@gmail.com>
2024-01-27 10:25:47 -0500

    backend ok

--- server/src/handlers/cart/addItemsHandler.js
+++ server/src/handlers/cart/addItemsHandler.js
@@ -2,18 +2,40 @@ const addItemsController = require("../../controllers/cart/addItemsController");
 
 const addItemsHandler = async (req, res) => {
   const { userId, productId } = req.params;
-  const { quantity } = req.body;
+  const { name, price, colors, size, quantity } = req.body;
+  console.log("userId:", userId);
+  console.log("productId:", productId);
+  console.log("name:", name);
+  console.log("quantity:", quantity);
+  console.log("price:", price);
+  console.log("colors:", colors);
+  console.log("size:", size);
   try {
-    const response = await addItemsController(userId, productId, quantity);
-    console.log("añadiendo items con estos datos:", userId, productId);
+    const response = await addItemsController(
+      userId,
+      productId,
+      quantity,
+      name,
+      price,
+      colors,
+      size
+    );
+    console.log(
+      "añadiendo items con estos datos:",
+      userId,
+      productId,
+      name,
+      price,
+      colors,
+      size,
+      quantity
+    );
     res.status(200).json(response);
   } catch (error) {
-    res
-      .status(404)
-      .json({
-        error: error.message || "No se econtraron items con este id:",
-        userId,
-      });
+    res.json({
+      error: error.message || "No se econtraron items con este id:",
+      userId,
+    });
   }
 };
 
