688d5b5a8024cd2ce00b4aacab9235fc6a58165f
AndresveraXX <andres_9_09@hotmail.com>
2024-01-31 11:49:06 -0300

    delete review

--- server/src/controllers/reviews/reviewsController.js
+++ server/src/controllers/reviews/reviewsController.js
@@ -4,11 +4,11 @@ const postReviews = async (req, res) => {
   try {
     // Extraer datos de la URL y el cuerpo de la solicitud
     const { idKey } = req.params;
-    const { content, rating, name, surName, profileImage } = req.body;
+    const { content, rating, name, profileImage } = req.body;
     console.log("esto viene de controlleer reviews", idKey, content, rating, profileImage)
 
     // Verifica que los datos necesarios estén presentes
-    if (!idKey || !content || !rating || !name || !surName || !profileImage ) {
+    if (!idKey || !content || !rating || !name || !profileImage) {
       return res.status(400).json({ error: 'Todos los campos son obligatorios.' });
     }
 
@@ -16,9 +16,8 @@ const postReviews = async (req, res) => {
       content,
       rating,
       name,
-      surName,
       profileImage,
-      productId: idKey
+      productId: idKey,
     });
 
     res.status(201).json({ success: true, review });
@@ -43,4 +42,61 @@ const getReviewsByProduct = async (req, res) => {
   }
 };
 
-module.exports = { postReviews, getReviewsByProduct };
+const deleteReviews = async (req, res) => {
+  try {
+    // Extraer el ID de la revisión y el ID del producto de los parámetros de la URL
+    const { productId, reviewId } = req.params;
+
+    // Verificar que ambos IDs estén presentes
+    if (!productId || !reviewId) {
+      return res.status(400).json({ error: 'ID de producto y/o ID de revisión no proporcionados.' });
+    }
+
+    // Busca la revisión por su ID y el ID del producto y elimínala
+    const deletedReviewsCount = await Review.destroy({
+      where: {
+        id: reviewId,
+        productId: productId,
+      },
+    });
+
+    // Verifica si se eliminó alguna revisión
+    if (deletedReviewsCount === 0) {
+      return res.status(404).json({ error: 'Revisión no encontrada.' });
+    }
+
+    // Devuelve una respuesta de éxito
+    res.status(200).json({ success: true, message: 'Revisión eliminada correctamente.' });
+  } catch (error) {
+    console.error('Error al eliminar la revisión:', error);
+    res.status(500).json({ error: 'Error interno del servidor.' });
+  }
+};
+
+
+const getReviewById = async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    
+    // Verificar si reviewId está presente
+    if (!reviewId) {
+      return res.status(400).json({ error: 'ID de revisión no proporcionado.' });
+    }
+
+    // Buscar la revisión por ID
+    const review = await Review.findByPk(reviewId);
+
+    // Verificar si se encontró la revisión
+    if (!review) {
+      return res.status(404).json({ error: 'Revisión no encontrada.' });
+    }
+
+    // Devolver la revisión encontrada
+    res.status(200).json(review);
+  } catch (error) {
+    console.error('Error al obtener la revisión por ID:', error);
+    res.status(500).json({ error: 'Error interno del servidor.' });
+  }
+};
+
+module.exports = { postReviews, getReviewsByProduct, deleteReviews, getReviewById};
--- server/src/controllers/users/loginController.js
+++ server/src/controllers/users/loginController.js
@@ -41,6 +41,7 @@ const login = async (req, res) => {
       country: user.country,
       profilePicture: user.profilePicture,
       paymentMethods: user.paymentMethods || null,
+      rol: user.rol,
     };
 
     
